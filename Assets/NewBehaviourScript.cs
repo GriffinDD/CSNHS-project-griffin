3d8d4fd6b4fc1318136910b319a19641f63981d8
Griffin Danner-Doran <griffindannerdoran@gmail.com>
2019-12-08 18:34:39 -0500

    first changes
    First commit to tower
    
    Signed-off-by: Griffin Danner-Doran <griffindannerdoran@gmail.com>

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 position = this.transform.position;
+            position.x = position.x - (float).02;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 position = this.transform.position;
+            position.x = position.x + (float).02;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 position = this.transform.position;
+            position.y = position.y + (float).03;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.localScale = new Vector3((float).2, (float).2, (float).2);
+        }
+        else
+        {
+            gameObject.transform.localScale = new Vector3((float).2, (float).4, (float).2);
+        }
+    }
+}
---
+++ Assets/enemyMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMove : MonoBehaviour
+{
+    public float loc;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = this.transform.position;
+        if (position.x ==  (float)1.22)
+        {
+             loc = (float)1;
+        }else if(position.x == (float)8.45)
+        {
+             loc = (float)0;
+        }
+        if(loc == (float)1)
+        {
+            position.x = position.x + (float).01;
+            this.transform.position = position;
+        }else if(loc == (float)0)
+        {
+            position.x = position.x - (float).01;
+            this.transform.position = position;
+        }
+    }
+}
