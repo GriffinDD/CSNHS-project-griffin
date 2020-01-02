e72b2244f556c23746f40e5b93106231481fc4d9
Griffin Danner-Doran <griffindannerdoran@gmail.com>
2020-01-02 17:43:40 -0500

    bullet

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -6,7 +6,7 @@ public class NewBehaviourScript : MonoBehaviour
 {
     Rigidbody2D rigidbody;
     public bool check = false;
-    public GameObject playerCube;
+    public GameObject bullet;
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Do something here");
@@ -20,6 +20,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        bullet = GameObject.Find("Bullet");
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -38,7 +39,7 @@ public class NewBehaviourScript : MonoBehaviour
             position.x = position.x + (float).02;
             this.transform.position = position;
         }
-        if (Input.GetKey(KeyCode.W)&& !check)
+        if (Input.GetKeyDown(KeyCode.W)&& !check)
         {
             check = true;
             this.rigidbody.velocity = Vector2.up * 4;  
@@ -51,5 +52,16 @@ public class NewBehaviourScript : MonoBehaviour
         {
             gameObject.transform.localScale = new Vector2((float).2, (float).4);
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector2 position = this.transform.position;
+            float xpos = position.x;
+            float ypos = position.y;
+            Vector2 newpos = new Vector2(xpos+(float).1,ypos);
+            GameObject clone;
+            clone = Instantiate(bullet);
+            clone.transform.position = newpos;
+            clone.GetComponent<Rigidbody2D>().velocity = 35 * transform.localScale.x * clone.transform.right;
+        }
     }
-}
+    }
